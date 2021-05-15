11f8f080f617b84223161143ed3d68eb567f1159
VToporan <80809332+VToporan@users.noreply.github.com>
2021-05-16 01:43:39 +0300

    ATSA-3 Minor bugfixes

--- src/main/java/Controllers/GeneralController.java
+++ src/main/java/Controllers/GeneralController.java
@@ -7,16 +7,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.net.URL;
 
 public class GeneralController {
 
     public void changeWindow(Button button, String windowName, int width, int height) {
         try {
-            Parent register_layout = FXMLLoader.load(getClass().getClassLoader().getResource(windowName));
-            Stage stage = (Stage) button.getScene().getWindow();
-            PauseTransition delay = new PauseTransition(Duration.seconds(1));
-            delay.setOnFinished( e -> stage.setScene(new Scene(register_layout, width, height)) );
-            delay.play();
+            URL resource = getClass().getClassLoader().getResource(windowName);
+            if (resource != null) {
+                Parent register_layout = FXMLLoader.load(resource);
+                Stage stage = (Stage) button.getScene().getWindow();
+                PauseTransition delay = new PauseTransition(Duration.seconds(1));
+                delay.setOnFinished(e -> stage.setScene(new Scene(register_layout, width, height)));
+                delay.play();
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
