52d88b73873ab3fe109fb190c5e14847c929251d
Tatucu <82051653+Tatucu@users.noreply.github.com>
2021-05-18 00:52:53 +0300

    Refactoring and HTML Editor

---
+++ module-info.java
@@ -0,0 +1,10 @@
+module ATAT.Study.App {
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.web;
+    requires javafx.fxml;
+    requires json.simple;
+    exports main;
+    exports Canvas;
+}
