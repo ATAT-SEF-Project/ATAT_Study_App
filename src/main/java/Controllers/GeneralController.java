d0ebc0ece02ea4c1d87ee6cac4358e317618933a
VToporan <80809332+VToporan@users.noreply.github.com>
2021-05-18 22:32:25 +0300

    ATSA-19 Adds function for opening window inside other window

--- src/main/java/Controllers/GeneralController.java
+++ src/main/java/Controllers/GeneralController.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.net.URL;
 
 public class GeneralController {
-
     public void changeWindow(Button button, String windowName, int width, int height) {
         try {
             URL resource = getClass().getClassLoader().getResource(windowName);
@@ -20,6 +21,19 @@ public class GeneralController {
                 PauseTransition delay = new PauseTransition(Duration.seconds(1));
                 delay.setOnFinished(e -> stage.setScene(new Scene(register_layout, width, height)));
                 delay.play();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void changeScene(BorderPane pane, String windowName) {
+        try {
+            URL resource = getClass().getClassLoader().getResource(windowName);
+            if (resource != null) {
+                Pane register_layout = FXMLLoader.load(resource);
+                pane.setCenter(register_layout);
             }
         }
         catch (Exception e) {
